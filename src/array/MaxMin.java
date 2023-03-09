ecba33f82d1547c3fc21b69fd022f48a7814e1d0
Richeek45 <richeek45@gmail.com>
2023-03-09 09:46:43 +0530

    binary method of searching max and min value

--- src/array/MaxMin.java
+++ src/array/MaxMin.java
@@ -7,6 +7,7 @@ public class MaxMin {
         int max;
     }
 
+    // Linear Method
     static Pair getMinMax (int arr[], int size) {
         Pair minMax = new Pair();
         int maxValue = minMax.max;
@@ -41,11 +42,57 @@ public class MaxMin {
 
     }
 
+    // Binary Method
+    static Pair getMinMax (int arr[], int start, int end) {
+        Pair minMax = new Pair();
+        Pair minMaxLeft = new Pair();
+        Pair minMaxRight = new Pair();
+
+        // if array size is 1
+        if (start == end) {
+            minMax.min = arr[0];
+            minMax.max = arr[0];
+            return minMax;
+        }
+
+        // if array size is 2
+        if (start + 1 == end) {
+            if (arr[start] > arr[end]) {
+                minMax.min = arr[end];
+                minMax.max = arr[start];
+            } else {
+                minMax.min = arr[start];
+                minMax.max = arr[end];
+            }
+            return minMax;
+        }
+
+        // if array size is more than 2
+        int mid = (start + end) / 2;
+        minMaxLeft = getMinMax(arr, start, mid);
+        minMaxRight = getMinMax(arr, mid + 1, end);
+
+        // find the min value between left and right
+        if (minMaxLeft.min < minMaxRight.min) {
+            minMax.min = minMaxLeft.min;
+        } else {
+            minMax.min = minMaxRight.min;
+        }
+
+        // find the max value between left and right
+        if (minMaxLeft.max > minMaxRight.max) {
+            minMax.max = minMaxLeft.max;
+        } else {
+            minMax.max = minMaxRight.max;
+        }
+
+        return minMax;
+    }
 
     public static void main (String args[]) {
         int array[] = {13, 15, 3, 21, 17, 49, 29};
         int size = array.length;
-        Pair minMax = getMinMax(array, size);
+        Pair minMax = getMinMax(array, 0, array.length-1);
         int maxValue = minMax.max;
         int minValue = minMax.min;
         System.out.println("Max Value: " + maxValue + ", Min Value: " + minValue);
