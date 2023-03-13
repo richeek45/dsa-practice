497f3297d65e63f2e40bf46a0d68c327f54f7b04
richeek45react <richeek@reactlabs.ai>
2023-03-13 22:31:34 +0530

    using set and TreeSet for sorted values and using iterator to get the max and min values

--- src/array/KthElementInArray.java
+++ src/array/KthElementInArray.java
@@ -1,6 +1,7 @@
 package array;
 
 import java.util.*;
+import java.lang.Integer;
 
 // Find the kth maximum and minimum element in an unsorted array
 public class KthElementInArray {
@@ -36,8 +37,37 @@ public class KthElementInArray {
         return minMax;
     }
 
-    // Using set DataStructure
+    // Using set DataStructure - Terrible Sol
+    static Pair getKthMinMaxSet(int arr[], int size, int k) {
+        Pair minMax = new Pair();
+        // for storing elements in a sorted form we are using TreeSet
+        Set<Integer> s = new TreeSet<Integer>();
 
+        // Since counting start from
+        int m = size - k;
+        int n = --k;
+
+        for (int i = 0; i < size; i++) {
+            s.add(arr[i]);
+        }
+
+        Iterator maxItr = s.iterator();
+        Iterator minItr = s.iterator();
+
+        while (m > 0) {
+            maxItr.next();
+            m--;
+        }
+
+        while (n > 0) {
+            minItr.next();
+            n--;
+        }
+
+        minMax.min = (int) minItr.next();
+        minMax.max = (int) maxItr.next();
+        return minMax;
+    }
 
 
     static void printArray (int arr[]) {
@@ -50,10 +80,11 @@ public class KthElementInArray {
 
     public static void main (String args[]) {
         int arr[] = {7, 10, 4, 3, 20, 15};
-        int k = 3;
+        int k = 3, size = arr.length;
         printArray(arr);
 //        Pair minMax = getKthMaxMin(arr, arr.length, k);
-        Pair minMax = getKthMinMaxSort(arr, arr.length, k);
+//        Pair minMax = getKthMinMaxSort(arr, size , k);
+        Pair minMax = getKthMinMaxSet(arr, size, k);
         printArray(arr);
         System.out.println("Min value = " + minMax.min + " and Max value = " + minMax.max + " with k = " + k);
     }
