424c0127bfdbe73014ac80d462c2c59a71ec0bb3
richeek45react <richeek@reactlabs.ai>
2023-03-19 13:09:01 +0530

    sort negative to positive using 2 pointer approach

--- src/array/SortNegativeToPositve.java
+++ src/array/SortNegativeToPositve.java
@@ -16,6 +16,25 @@ public class SortNegativeToPositve {
         }
     }
 
+    // Two pointer approach
+    static void negativeToPostiveUsingTwoPointer (int arr[], int left, int right) {
+        while (left <= right) {
+            if (arr[left] < 0 && arr[right] < 0) {
+                left++;
+            } else if (arr[left] > 0 && arr[right] < 0) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            } else if (arr[left] > 0 && arr[right] > 0) {
+                right--;
+            } else {
+                // when the elements are in correct order arr[left] < 0 && arr[right] > 0
+                left++;
+                right--;
+            }
+        }
+    }
+
     static void swap (int arr[], int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
@@ -31,7 +50,8 @@ public class SortNegativeToPositve {
     public static void main (String args[]) {
         int arr[] = { 1, 2,  -4, -5, 2, -7, 3, 2, -6, -8, -9, 3, 2,  1 };
         int size = arr.length;
-        negativeToPositiveUsingDNF(arr, size);
+        // negativeToPositiveUsingDNF(arr, size);
+        negativeToPostiveUsingTwoPointer(arr, 0, size - 1);
         printArray(arr, size);
 
     }
