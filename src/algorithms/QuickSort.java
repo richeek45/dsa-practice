a956689895cb4bfd425b997f934c90105bf6aed9
Richeek45 <richeek45@gmail.com>
2023-03-19 12:57:44 +0530

    quick sort by hoare method

--- src/algorithms/QuickSort.java
+++ src/algorithms/QuickSort.java
@@ -11,7 +11,8 @@ public class QuickSort {
         arr[j] = temp;
     }
 
-    static int partition (int arr[], int low, int high) {
+    // Quick Sort by Lomuto Method
+    static int partitionByLomuto(int arr[], int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j <= high - 1; j++) {
@@ -25,12 +26,42 @@ public class QuickSort {
         return (i + 1);
     }
 
-    static void quickSort (int arr[], int low, int high) {
+    static void quickSortByLomutoMethod(int arr[], int low, int high) {
 
         if (low < high) {
-            int partitionIndex = partition(arr, low, high);
-            quickSort(arr, low, partitionIndex - 1);
-            quickSort(arr, partitionIndex + 1, high);
+            int partitionIndex = partitionByLomuto(arr, low, high);
+            quickSortByLomutoMethod(arr, low, partitionIndex - 1);
+            quickSortByLomutoMethod(arr, partitionIndex + 1, high);
+        }
+    }
+
+    // Quick Sort by Hoare Method
+    static int partitionByHoare(int arr[], int low, int high) {
+        int i = low - 1, j = high + 1;
+        int pivot = arr[low];
+
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+
+            swap(arr, i, j);
+        }
+    }
+
+    static void quickSortByHoareMethod (int arr[], int low, int high) {
+        if (low < high) {
+            int partitionIndex = partitionByHoare(arr, low, high);
+            quickSortByHoareMethod(arr, low, partitionIndex);
+            quickSortByHoareMethod(arr, partitionIndex + 1, high);
         }
     }
 
@@ -44,7 +75,8 @@ public class QuickSort {
     public static void main (String args[]) {
         int arr[] = {10, 80, 30, 90, 40, 50, 70};
         int size = arr.length;
-        quickSort(arr, 0, size - 1);
+        // quickSortByLomutoMethod(arr, 0, size - 1);
+        quickSortByHoareMethod(arr, 0, size - 1);
         printArray(arr, size);
     }
 }
