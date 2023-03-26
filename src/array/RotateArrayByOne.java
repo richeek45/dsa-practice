cdd8b8bd7e1e08d8a668d0e8ac7f48d4fba04cc9
Richeek45 <richeek45@gmail.com>
2023-03-26 21:36:43 +0530

    rotate an array by k times

---
+++ src/array/RotateArrayByOne.java
@@ -0,0 +1,66 @@
+package array;
+
+// Cyclically rotate an array by one
+public class RotateArrayByOne {
+
+    static void rotateOne (int arr[], int size) {
+        int lastValue = arr[size - 1];
+        for (int i = size - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = lastValue;
+    }
+
+    static void rotateOneByReverse (int arr[], int size) {
+        int i = 0, j = size - 1;
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+        }
+    }
+
+    static void swap (int arr[], int i,int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void rotateArrayKtimes (int arr[], int size, int k) {
+
+        // rotate the first n - 1 terms for k = 1 and n - k - 1 for k times
+        for (int i = 0, j = size - k - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+        printArray(arr, size);
+
+        // reverse the entire array
+        for (int i = 0, j = size - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+
+        for (int i = 0, j = k - 1; i < j; i++, j--) {
+            swap(arr, i, j);
+        }
+
+    }
+
+    static void printArray(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main (String args[]) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int size = arr.length;
+        int k = 4;
+        // rotateOne(arr, size);
+        // rotateOneByReverse(arr, size);
+        rotateArrayKtimes(arr, size, k);
+        printArray(arr, size);
+    }
+}
