b655a1223cc10d48bb7428ceb331c353651a442e
richeek45 <richeek45@gmail.com>
2023-07-02 15:57:35 +0530

    lps taking center index as starting point oftraversal and finding the maximum palindrome substring

--- src/strings/LongestPalindromeSubString.java
+++ src/strings/LongestPalindromeSubString.java
@@ -83,12 +83,56 @@ public class LongestPalindromeSubString {
         return str.substring(start, start + maxLength);
     }
 
+    static void lps3(String str) {
+        //Maintain a variable maxLength = 1 (for storing LPS length) and start =0 (for storing starting index of LPS ).
+        //The idea is very simple, we will traverse through the entire string with i=0 to i<(length of string).
+        //while traversing, initialize low and high pointer such that low= i-1 and high= i+1.
+        //keep incrementing ‘high’ until str[high]==str[i] .
+        //similarly keep decrementing ‘low’ until str[low]==str[i].
+        //finally we will keep incrementing ‘high’ and decrementing ‘low’ until str[low]==str[high].
+        //calculate length=high-low-1, if length > maxLength then maxLength = length and start = low+1 .
+        int maxLength = 1, start = 0;
+        int len = str.length();
+        int high = len, low = 0;
+
+        for (int i = 0; i < len; i++) {
+            high = i+1;
+            low = i-1;
+
+            // increasing high index until character at index i and high matches
+            while (high < len && str.charAt(i) == str.charAt(high)) {
+                high++;
+            }
+            // decreasing low index until character at index i and low index matches
+            while (low >= 0 && str.charAt(i) == str.charAt(low)) {
+                low--;
+            }
+            // taking index at i as center of the palindrome check the adjacent characters
+            // if characters match continuously check low-- and high++ characters until they do not match
+            while (high < len && low >= 0 && str.charAt(high) == str.charAt(low)) {
+                high++;
+                low--;
+            }
+
+            // Currently low and high are out of index of palindrome
+            // starts from low+1 and ends in high - 1
+            int length = high - low - 1;
+            if (maxLength < length) {
+                maxLength = length;
+                start = low + 1;
+            }
+        }
+
+        System.out.println(start + " " + maxLength +  " " + str.substring(start, start + maxLength));
+    }
+
     public static void main(String[] args) {
         String str = "forgeeksskeegfor";
         String str2 = "ABBDCACB";
 
 //        lpsRecursion(str, 0, str.length() - 1);
-        String subString = lpsUsingDP2(str);
-        System.out.println(subString);
+//        String subString = lpsUsingDP2(str);
+//        System.out.println(subString);
+        lps3(str);
     }
 }
