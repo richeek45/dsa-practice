588c20d6aef1415496cf2aa6a81291135f4b793f
richeek45 <richeek45@gmail.com>
2023-07-10 19:19:20 +0530

    finding spiralMatrix by dividing into cycles

--- src/matrix/SpiralMatrix.java
+++ src/matrix/SpiralMatrix.java
@@ -1,5 +1,6 @@
 package matrix;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class SpiralMatrix {
@@ -35,12 +36,50 @@ public class SpiralMatrix {
         System.out.println(ans);
     }
 
+    // printing by dividing matrix into cycles or loops
+    static void printSpiralMatrix2(int[][] matrix) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int rowLength = matrix.length, colLength = matrix[0].length;
+        int rowIndex = 0, colIndex = 0;
+
+        while (rowIndex < rowLength && colIndex < colLength) {
+            // print the row line in right direction along each column
+            for (int i = colIndex; i < colLength; i++) {
+                res.add(matrix[rowIndex][i]);
+            }
+            rowIndex++;
+
+            // print the column line in the bottom direction
+            for (int i = rowIndex; i < rowLength; i++) {
+                res.add(matrix[i][colLength - 1]);
+            }
+            colLength--;
+            if (rowIndex < rowLength) {
+                // print the row line in left direction
+                for (int i = colLength - 1; i >= colIndex; i--) {
+                    res.add(matrix[rowLength - 1][i]);
+                }
+                rowLength--;
+            }
+
+            if (colIndex < colLength) {
+                for (int i = rowLength - 1; i >= rowIndex; i--) {
+                    res.add(matrix[i][colIndex]);
+                }
+                colIndex++;
+            }
+
+        }
+        System.out.println(res);
+    }
+
 
     public static void main(String[] args) {
-        int[][] matrix =   {{1,   2,   3,   4},
-                            {5,   6,   7,   8},
-                            {9,   10,  11,  12},
-                            {13,  14,  15,  16}};
-        printSpiralMatrix(matrix);
+        int[][] matrix =   {{1,   2,   3,   4,  18},
+                            {5,   6,   7,   8,  19},
+                            {9,   10,  11,  12, 20},
+                            {13,  14,  15,  16, 21}};
+//        printSpiralMatrix(matrix);
+        printSpiralMatrix2(matrix);
     }
 }
