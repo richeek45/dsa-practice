1c58acc3c617ec3a5ffb7217959662aaa133ad4c
richeek45 <richeek45@gmail.com>
2023-08-30 16:05:26 +0530

    flattening the 2d linkedlist with comparing the two right pointer head nodes with the vertical linked lists

--- src/LinkedList/FlattenLL.java
+++ src/LinkedList/FlattenLL.java
@@ -49,6 +49,51 @@ public class FlattenLL {
         }
     }
 
+    private static Node merge(Node left, Node right) {
+        // compare two vertical linkedlist with left and right as two head of the linkedlist
+        // and create a sorted linked list
+
+        // while comparing and calling the down node of the linkedlists if any one of the nodes are null
+        // it should return the node of the other ll
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+
+        Node result = null;
+        if (left.data <= right.data) {
+            result = left;
+            result.down = merge(left.down, right);
+        } else {
+            result = right;
+            result.down = merge(left, right.down);
+        }
+        // making the right pointer to null because left and right nodes have right pointer attached to it
+        result.right = null;
+
+        return result;
+    }
+
+    static Node flattenLL(Node root) {
+        // call the right node recursively till it hit the reaches the end of the list
+        // call merge on the two vertical nodes and compare recusively and create a 1D linkedlist
+        // with down as the reference to the next node
+        if (root == null || root.right == null) {
+            return root;
+        }
+
+        // recur for list on the right
+        root.right = flattenLL(root.right);
+
+        // create a 1D linked list with two right adjacent nodes with down linked list
+        root = merge(root, root.right);
+
+        // sorted linkedlist between two right heads is returned to the previous right head
+        return root;
+    }
+
     public static void main(String[] args) {
         int[][] arr = {{5, 7, 8, 30}, {10, 20}, {19, 22, 15}, {28, 35, 40, 45}};
         int row = arr[0].length;
@@ -58,7 +103,8 @@ public class FlattenLL {
             head = insert(head, arr[i], i);
         }
         printList(head);
-
-
+        System.out.println();
+        Node newHead = flattenLL(head);
+        printList(newHead);
     }
 }
