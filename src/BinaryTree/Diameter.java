7296a9f31ab98af5066aaaed7532f22877ff31e7
richeek45 <richeek45@gmail.com>
2023-09-02 14:54:50 +0530

    finding the diameter of a tree using Morris Traversal method

--- src/BinaryTree/Diameter.java
+++ src/BinaryTree/Diameter.java
@@ -61,6 +61,59 @@ public class Diameter {
         return Math.max(leftHeight.h + rightHeight.h + 1, Math.max(leftDiameter, rightDiameter));
     }
 
+    static int getDiameter3(Node root) {
+        // Using Morris Traversal Algorithm
+        // Define a struct or class for a binary tree node that contains data, a pointer to its left child, and a pointer to its right child.
+        // Initialize the current node as the root of the binary tree.
+        // While the current node is not NULL, do the following:
+        // a. If the left child of the current node is NULL, move to the right child.
+        // b. If the left child of the current node is not NULL, find the rightmost node in the left subtree of the current node.
+        // c. If the right child of the rightmost node is NULL, set its right child to the current node, and move to the left child of the current node.
+        // d. If the right child of the rightmost node is not NULL, set its right child back to NULL, visit the current node, and move to its right child.
+        // e. For each visited node, calculate its left and right subtree heights using the maximum function
+        // and update the diameter as the maximum of the sum of their heights and the current diameter.
+        // Return the diameter of the binary tree.
+
+
+        Node curr = root;
+        int ans = 0;
+
+        if (curr != null) {
+            if (curr.left == null) {
+                curr = curr.right;
+            } else {
+                // left child is not null
+                // find the rightmost node of the left-subtree
+                Node pre = curr.left;
+                while(pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    // pre.right is not null
+                    pre.right = null;
+                    int leftHeight = 0, rightHeight = 0;
+                    Node temp = curr.left; // finding the height of the left-subtree and the righ-tsubtree
+                    while (temp != null) {
+                        leftHeight++;
+                        temp = temp.right;
+                    }
+                    temp = curr.right;
+                    while(temp != null) {
+                        rightHeight++;
+                        temp = temp.left;
+                    }
+                    ans = Math.max(ans, leftHeight + rightHeight + 1);
+                    curr = curr.right;
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         Node tree = new Node(1);
