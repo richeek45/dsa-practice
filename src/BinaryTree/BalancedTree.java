b346aef620f3d680b9be8b60fe49cffad7bccccd
richeek45 <richeek45@gmail.com>
2023-09-04 18:00:07 +0530

    checking balancedTree using pair class

--- src/BinaryTree/BalancedTree.java
+++ src/BinaryTree/BalancedTree.java
@@ -1,6 +1,15 @@
 package BinaryTree;
 
 public class BalancedTree {
+    static class Pair {
+        boolean isBalanced;
+        int height;
+        Pair(boolean val, int h) {
+            isBalanced = val;
+            height = h;
+        }
+    }
+
     static class Node {
         int data;
         Node left;
@@ -39,6 +48,7 @@ public class BalancedTree {
     }
 
     static int isBalancedTree2(Node root) {
+        // O(N)
         // calculating height on the same recursion tree
         if (root == null) {
             return 0;
@@ -54,6 +64,30 @@ public class BalancedTree {
         return -1;
     }
 
+    static Pair isBalancedTree3(Node root) {
+        // Using a Pair to store a boolean if balanced and second parameters as a height
+        if (root == null) {
+            Pair p = new Pair(true, 0);
+            return p;
+        }
+
+        Pair left = isBalancedTree3(root.left);
+        Pair right = isBalancedTree3(root.right);
+
+        boolean leftAns = left.isBalanced;
+        boolean rightAns = right.isBalanced;
+
+        boolean checkBalanced = Math.abs(left.height - right.height) <= 1;
+
+        // initializing ans with false and height
+        Pair ans = new Pair(false, Math.max(left.height, right.height) + 1);
+
+        if (leftAns && rightAns && checkBalanced) {
+            ans = new Pair(true, Math.max(left.height, right.height) + 1);
+        }
+
+        return ans;
+    }
 
     public static void main(String[] args) {
         // A height balanced binary tree is a binary tree in which the height of the
@@ -73,6 +107,7 @@ public class BalancedTree {
         root1.left.left.right = new Node(5);
 
         boolean isBalanced = isBalancedTree2(root) != -1 ? true : false;
-        System.out.println(isBalanced);
+        boolean isBalanced2 = isBalancedTree3(root).isBalanced;
+        System.out.println(isBalanced2);
     }
 }
