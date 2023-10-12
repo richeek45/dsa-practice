5b47de568942738abdb5e21cfb2b005c4361ebd1
richeek45 <richeek45@gmail.com>
2023-10-12 17:47:29 +0530

    coloring graph node using bfs

--- src/Graph/GraphNodeColoring.java
+++ src/Graph/GraphNodeColoring.java
@@ -50,6 +50,43 @@ public class GraphNodeColoring {
         }
     }
 
+    public static void graphColoring2(int[][] graph) {
+        int[] colors = new int[V];
+        Arrays.fill(colors, -1);
+        colors[0] = 0;
+        // colors that are available
+        boolean[] available = new boolean[V];
+        // initially all colors are available to a vertex
+        Arrays.fill(available, true);
+        available[0] = false;
+
+        // selecting colors for the next vertices after 0
+        for(int i = 1; i < V; i++) {
+            // i is the vertex we are assigning color, j is the adjacent vertex
+            for(int j = 0; j < V; j++) {
+                if (graph[i][j] == 1 && colors[j] != -1) {
+                    available[colors[j]] = false;
+                }
+            }
+
+            // find the first available color
+            int color;
+            for(color = 0; color < V; color++) {
+                if (available[color]) {
+                    break;
+                }
+            }
+            colors[i] = color;
+            // Making the available colors to true for the next adjacent vertices
+            Arrays.fill(available, true);
+        }
+
+        for(int i = 0; i < V; i++) {
+            System.out.printf("%2d", colors[i]);
+        }
+
+    }
+
     private static void printColor(int[] colors) {
         for(int color: colors) {
             System.out.print(color + " ");
@@ -67,15 +104,21 @@ public class GraphNodeColoring {
               (0)---(1)
         */
         int[][] graph = {
-                { 0, 1, 1, 1 },
-                { 1, 0, 1, 0 },
-                { 1, 1, 0, 1 },
-                { 1, 0, 1, 0 },
+                // 0 -> 1, 2
+                // 1 -> 0, 2, 3
+                // 2 -> 0, 1, 3
+                // 3 -> 1, 2, 4
+                { 0, 1, 1, 0, 0 },
+                { 1, 0, 1, 1, 0},
+                { 1, 1, 0, 1, 0 },
+                { 0, 1, 1, 0, 1 },
+                { 0, 0, 0, 1, 0 },
         };
         V = graph.length;
 
         int m = 3;
-        graphColoring(graph, 3);
+//        graphColoring(graph, m);
+        graphColoring2(graph);
 
     }
 }
