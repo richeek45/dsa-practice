1e353e4ddc00bda7c70e31f68cb21c8ad0a84ec0
richeek45 <richeek45@gmail.com>
2023-10-14 14:26:25 +0530

    Rat maze solution when rat can make multiple moves at a time

--- src/backtracking/RatInAMaze.java
+++ src/backtracking/RatInAMaze.java
@@ -83,7 +83,7 @@ public class RatInAMaze {
         int[] rowX = { 0, 1, 0, -1 };
         int[] colY = { 1, 0, -1, 0 };
         String[] move = {"R", "D", "L", "U" };
-
+        // notion-db -> secret_ziaN2JF0D6jy99ifiQGTQxTJCWUaTZ0kvkNZ8lEaN6X
         // Making the position 0 so that it doesn't traverse again
         matrix[x][y] = 0;
 
@@ -96,26 +96,84 @@ public class RatInAMaze {
         // moving it back it 1 after backtracking to the loop,
         // so that it can be traversed by other loops
         matrix[x][y] = 1;
-        return;
+    }
+
+    private static boolean isSafe(int[][] matrix, int x, int y, int row, int col) {
+        if(x >= 0 && x < row && y >= 0 && y < col && matrix[x][y] > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // Solution for rat in a maze when multiple moves are allowed
+    // The non-zero value of mat[i][j] indicates number of maximum jumps rat can make from cell mat[i][j].
+    public static boolean getAllPath3(int[][] matrix, int x, int y, int row, int col, int[][] sol) {
+        // destination is reached
+        if (x == row-1 && y == col-1) {
+            sol[x][y] = 1;
+            return true;
+        }
+
+        if (!isSafe(matrix, x, y, row, col)) {
+            return false;
+        }
+
+        // marking the x, y coordinates as part of the solution
+        sol[x][y] = 1;
+        // for the number of possible moves in matrix[x][y]
+        int moves = matrix[x][y];
+        for(int i = 1; i <= moves; i++) {
+            // moving in horizontal y direction
+            if(getAllPath3(matrix, x, y + i, row, col, sol)) {
+                return true;
+            }
+
+            // moving in vertical x direction
+            if(getAllPath3(matrix, x + i, y, row, col, sol)) {
+                return true;
+            }
+        }
+
+        // backtracking to previous sol to find other possible solutions
+        sol[x][y] = 0;
+        return false;
+    }
+
+    public static void printSol(int[][] sol) {
+        for(int i = 0; i  < sol.length; i++) {
+            for(int j = 0; j < sol[0].length; j++) {
+                System.out.print(sol[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
+//        int[][] matrix = {
+//                { 1, 0, 1, 1, 1 },
+//                { 1, 1, 1, 0, 1 },
+//                { 1, 1, 0, 0, 1 },
+//                { 0, 1, 1, 0, 1 },
+//                { 0, 0, 1, 1, 1 } };
         int[][] matrix = {
-                { 1, 0, 1, 1, 1 },
-                { 1, 1, 1, 0, 1 },
-                { 1, 1, 0, 0, 1 },
-                { 0, 1, 1, 0, 1 },
-                { 0, 0, 1, 1, 1 } };
-
+                {2, 1, 0, 0},
+                {3, 0, 0, 1},
+                {0, 1, 0, 1},
+                {0, 0, 0, 1}};
         int x = 0, y = 0;
         int row = matrix.length;
         int col = matrix[0].length;
-        List<String> result = new ArrayList<>();
+//        List<String> result = new ArrayList<>();
 //        getAllPath(matrix, 0 , 0, row, col, result, "");
-        getAllPath2(matrix, 0 , 0, row, col, result);
-        for(String str : result) {
-            System.out.print(str + " ");
-        }
+//        getAllPath2(matrix, 0 , 0, row, col, result);
+//        for(String str : result) {
+//            System.out.print(str + " ");
+//        }
+
+        int[][] sol = new int[row][col];
+        printSol(matrix);
+        getAllPath3(matrix, 0, 0, row, col, sol);
+        printSol(sol);
 
     }
 }
