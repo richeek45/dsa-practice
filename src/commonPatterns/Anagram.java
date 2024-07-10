b9e94efb5c29f15fcd1304ba6370daa52e74a7b6
richeek45 <richeek45@gmail.com>
2024-07-10 16:30:16 +0530

    anagram using char count

--- src/commonPatterns/Anagram.java
+++ src/commonPatterns/Anagram.java
@@ -14,8 +14,28 @@ public class Anagram {
         return Arrays.equals(s1, s2);
     }
 
+    public static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "anagram", t = "nagaram";
-        System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram2(s, t));
     }
 }
