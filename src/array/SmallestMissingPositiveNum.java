656bbd29efa73d68e8acd0c7e4e3bc7acb2b2e05
richeek45 <richeek45@gmail.com>
2025-02-06 14:28:49 +0530

    using cycle sort to swap the element to its correct position in the array

--- src/array/SmallestMissingPositiveNum.java
+++ src/array/SmallestMissingPositiveNum.java
@@ -37,12 +37,43 @@ public class SmallestMissingPositiveNum {
         }
 
         System.out.println(res);
+    }
 
+    static void missingPositiveNumber2(int[] arr) {
+        int len = arr.length;
+        // arrange the number to its correct position
+        // check arr[i], if not i+1, then iterate and searcb i+1 and swap both the values
+        // for arr[i], we check if the value is arr[i] = i + 1,
+        // if not then we put the value in its correct position
+        // if arr[i] = 4, it should be in arr[i]-1= 4-1=3 index
+        // i.e. arr[i] should be in arr[arr[i] - 1]
+        // so arr[arr[i]-1] should be swapped with arr[i] at i position
+        // we need while here because after swapping, if the arr[i] is still misplaced then
+        // we need to swap again to its correct position
+
+        for (int i = 0; i < len; i++) {
+            while ((arr[i] >= 1 && arr[i] <= len) && (arr[i] != arr[arr[i] - 1])) {
+                int temp = arr[i];
+                arr[i] = arr[arr[i]-1];
+                arr[temp-1] = temp;
+            }
+        }
+        int res = 0;
+        // to check the first number not present in array
+        for (int i = 1; i <= len; i++) {
+            if (arr[i-1] != i) {
+                res = i;
+                break;
+            }
+        }
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
         int[] arr = {2, -3, 4, 1, 1, 7};
+        int[] arr2 = {3, 4, -1, 1};
 //        missingPositiveNumber(arr);
-        missingPositiveNumber1(arr);
+//        missingPositiveNumber1(arr);
+        missingPositiveNumber2(arr);
     }
 }
